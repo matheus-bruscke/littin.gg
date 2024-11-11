30cd34d17927cd0526fe3458e704ceb0022db16a
Matheus Littig <littig.works@gmail.com>
2024-11-11 19:29:35 -0400

    chore: extend Tailwind CSS content paths to include component files for improved styling coverage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {},
   plugins: [],
 };
