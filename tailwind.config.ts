ee82fc7efb2e5ebfccc1cc61d8a524fa67362e3d
Matheus Littig <littig.works@gmail.com>
2024-11-27 20:20:15 -0400

    feat: add react-icons dependency; introduce slide-in animation keyframes and styles for enhanced UI transitions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,16 @@ const config: Config = {
       gridTemplateColumns: {
         "layout-desktop": "364px 1fr",
       },
+      keyframes: {
+        "slide-in": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+
+      animation: {
+        "slide-in": "slide-in 0.2s ease",
+      },
     },
   },
   plugins: [],
