90eb18f1953399b57063bd9fb42d3a681f7d4f39
Matheus Littig <littig.works@gmail.com>
2024-11-30 19:37:09 -0400

    feat: add inner-lg box shadow and slide-out animation; update slide-in keyframes for smoother UI transitions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,18 +7,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        "inner-lg": "inset 0 -2px 4px rgba(0, 0, 0, 0.6)",
+      },
       gridTemplateColumns: {
         "layout-desktop": "364px 1fr",
       },
       keyframes: {
         "slide-in": {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(0)" },
+          "0%": { transform: "translateX(10%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-out": {
+          "0%": { transform: "translateX(0)", opacity: "1" },
+          "100%": { transform: "translateX(10%)", opacity: "0" },
         },
       },
 
       animation: {
         "slide-in": "slide-in 0.2s ease",
+        "slide-out": "slide-out 0.2s ease",
       },
     },
   },
